0b743911825b3d67e4cbcf2996f452bcf5a50380
RyanLu0701 <112445233+RyanLu0701@users.noreply.github.com>
2022-11-03 20:42:56 +0800

    modified argparse and add scheduler.py and add path

---
+++ scheduler.py
@@ -0,0 +1,7 @@
+import os
+os.system("python main.py --epochs=50 --epochs_ft=50 --lr=0.01 --lr_ft=0.001")
+os.system("python main.py --epochs=50 --epochs_ft=50 --lr=0.01 --lr_ft=0.01")
+os.system("python main.py --epochs=50 --epochs_ft=50 --lr=0.001 --lr_ft=0.001")
+os.system("python main.py --epochs=100 --epochs_ft=100 --lr=0.01 --lr_ft=0.001")
+os.system("python main.py --epochs=200 --epochs_ft=100 --lr=0.01 --lr_ft=0.001")
+
